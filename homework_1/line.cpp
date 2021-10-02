bf9221bf25b0688c71e9aa56a33d1a101cab1fc9
HvarZ <hvarzahar@gmail.com>
2021-10-02 12:40:11 +0300

    delete vector realization; mitigate memory cost

--- homework_1/line.cpp
+++ homework_1/line.cpp
@@ -4,11 +4,12 @@
 #include <SFML/Graphics.hpp>
 
 
-std::vector<sf::RectangleShape> BresenhamLine(sf::Vector2<int>& firstPoint,
+void PrintBresenhamLine(sf::Vector2<int>& firstPoint,
                    sf::Vector2<int>& secondPoint,
-                   const float sizePoint = 10.f) noexcept {
+                   const float sizePoint = 10.f,
+                   sf::RenderWindow* window = nullptr) noexcept {
     bool steep = std::abs(secondPoint.y - firstPoint.y) > std::abs(secondPoint.x - firstPoint.x);
-
+    window->clear();
     if (steep) {
         std::swap(firstPoint.x, firstPoint.y);
         std::swap(secondPoint.x, secondPoint.y);
@@ -25,38 +26,36 @@ std::vector<sf::RectangleShape> BresenhamLine(sf::Vector2<int>& firstPoint,
     float yStep = (firstPoint.y < secondPoint.y) ? sizePoint : -sizePoint;
     float y = firstPoint.y;
 
-    std::vector<sf::RectangleShape> points(((secondPoint.x - firstPoint.x) / static_cast<int>(sizePoint)) + 1);
-
-    for (auto& point : points) {
-        point.setFillColor(sf::Color::White);
-        point.setSize(sf::Vector2f(sizePoint, sizePoint));
-    }
+    sf::RectangleShape point;
+    point.setFillColor(sf::Color::White);
+    point.setSize(sf::Vector2f(sizePoint, sizePoint));
     int i = 0;
 
     for (auto x = firstPoint.x; x <= secondPoint.x; x += sizePoint, i++) {
-        points[i].setPosition(steep ? y : x, steep ? x : y);
+        point.setPosition(steep ? y : x, steep ? x : y);
+        window->draw(point);
         error -= dy;
         if (error < 0) {
             y += yStep;
             error += dx;
         }
     }
-    return points;
+    window->display();
 }
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Bresenham Line");
     window.setFramerateLimit(60);
+    window.display();
     bool isClickMouse = false;
 
     sf::Vector2<int> point1;
     sf::Vector2<int> point2;
-    decltype(BresenhamLine(point1, point2)) line;
 
     while (window.isOpen()) {
         sf::Event event{};
 
-        while (window.pollEvent(event)) {
+        while (window.waitEvent(event)) {
             if ((event.type == sf::Event::Closed) ||
                 (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
                 window.close();
@@ -66,16 +65,12 @@ int main() {
                     isClickMouse = true;
                 } else {
                     point2 = sf::Mouse::getPosition(window);
-                    line = BresenhamLine(point1, point2, 10);
+                    PrintBresenhamLine(point1, point2, 10, &window);
                     isClickMouse = false;
                 }
             }
         }
 
-        for (const auto& point : line) {
-            window.draw(point);
-        }
-        window.display();
     }
     return 0;
 }
